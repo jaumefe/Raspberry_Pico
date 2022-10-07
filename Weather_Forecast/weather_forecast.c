2d65f0aa0232dc07a0eedf4f696a09cd374ff151
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2022-10-07 16:19:44 +0200

    I2C basic config

--- Weather_Forecast/weather_forecast.c
+++ Weather_Forecast/weather_forecast.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/i2c.h"
 #include "pico/binary_info.h"
 
+//I2C reserves some addresses for special purposes.
+    //These are any addresses of the form 000 0xxx or 111 1xxx
+bool reserved_addr(uint8_t addr){
+        return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+    }
+
+void configureI2C(){
+#if !defined (i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+#warning i2c/bus_scan example requires a board with I2C pins
+    puts("Default I2C pins were not defined");
+#else
+    // In this case we will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
+    i2c_init(i2c_default, 10 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+}
+
 int main (){
+    //Enable UART so status output can be printed
+    stdio_init_all();
+
+    //Configuration of I2C. I2C pins are available to picotool
+    configureI2C();
+
     while(1){
         
     }
