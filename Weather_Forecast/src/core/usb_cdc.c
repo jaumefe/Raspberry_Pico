463b2b4794e43750e4cd53e7873c309629357dca
jaumefe <jaumef.escrig96@gmail.com>
2025-10-05 20:58:20 +0200

    weather forecast: add cdc usb communication

---
+++ Weather_Forecast/src/core/usb_cdc.c
@@ -0,0 +1,98 @@
+#include "pico/stdlib.h"
+#include "tusb.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include <string.h>
+
+const uint LED_USB_PIN = 1;
+
+static QueueHandle_t usb_rx_queue = NULL;
+
+#define USB_RX_QUEUE_SIZE 1024
+static char rx_buffer[USB_RX_QUEUE_SIZE];
+
+void usb_task(void *param) {
+    (void) param;
+    while (1) {
+        tud_task();
+        vTaskDelay(pdMS_TO_TICKS(1));
+    }
+}
+
+void usb_init(void){
+    tusb_init();
+    stdio_usb_init();
+
+    xTaskCreate(usb_task, "usb_task", 1024, NULL, configMAX_PRIORITIES - 1, NULL);
+    return;
+}
+
+void tud_cdc_rx_cb(uint8_t itf) {
+    (void) itf;
+    while (tud_cdc_available()) {
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+        size_t count = tud_cdc_read(rx_buffer, sizeof(rx_buffer));
+        if (usb_rx_queue != NULL) {
+            for (size_t i = 0; i < count; i++) {
+                xQueueSendFromISR(usb_rx_queue, &rx_buffer[i], &xHigherPriorityTaskWoken);
+            }
+        }
+    }
+}
+
+void usb_cdc_task(void *p) {
+    (void) p;
+    char cmd_buf[128]= {0};
+    size_t idx = 0;
+    char c;
+    gpio_init(LED_USB_PIN);
+	gpio_set_dir(LED_USB_PIN, GPIO_OUT);
+
+    while (1) {
+        if (xQueueReceive(usb_rx_queue, &c, portMAX_DELAY) == pdTRUE) {
+            if (c != '\n' && c != '\r') {
+                if (idx < sizeof(cmd_buf) - 1) {
+                    cmd_buf[idx++] = c;
+                } else {
+                    idx = 0;
+                    cmd_buf[0] = '\0';
+                }
+            } else if (c == '\n') {
+                cmd_buf[idx] = '\0';
+                if (strcmp(cmd_buf, "LED_ON") == 0) {
+                    gpio_put(LED_USB_PIN, 1);
+                    tud_cdc_write_str("LED turned ON\r\n");
+                } else if (strcmp(cmd_buf, "LED_OFF") == 0) {
+                    gpio_put(LED_USB_PIN, 0);
+                    tud_cdc_write_str("LED turned OFF\r\n");
+                } else {
+                    tud_cdc_write_str("Unknown command: ");
+                    tud_cdc_write_str(cmd_buf);
+                    tud_cdc_write_str("\r\n");
+                }
+                tud_cdc_write_flush();
+
+                idx = 0;
+                cmd_buf[0] = '\0';
+            }
+        }  
+    }
+}
+
+void cdc_init(void){
+    usb_rx_queue = xQueueCreate(128, sizeof(char));
+    if (usb_rx_queue == NULL) {
+        // Handle error
+        while (1);
+    }
+    xTaskCreate(usb_cdc_task, "usb_cdc_task", 1024, NULL, tskIDLE_PRIORITY+1, NULL);
+    return;
+}
+
+void usb_cdc_init(void){
+    usb_init();
+    cdc_init();
+
+    return;
+}
--- Weather_Forecast/src/weather_forecast.c
+++ Weather_Forecast/src/weather_forecast.c
@@ -6,9 +6,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "modules/led.h"
+#include "core/usb_cdc.h"
+
 
 
 int main (void){
+	stdio_init_all();
+	usb_cdc_init();
     xTaskCreate(blinkHandler, "blinker", 1024, NULL, 1, NULL);
     vTaskStartScheduler();
 	
