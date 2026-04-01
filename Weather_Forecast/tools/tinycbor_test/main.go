919d0c5523caaf0a756ac3680b2a0c5bf6d9c946
jaumefe <jaumef.escrig96@gmail.com>
2026-04-01 19:35:55 +0200

    weather_forecast tool to test tinycbor

--- Weather_Forecast/tools/tinycbor_test/main.go
+++ Weather_Forecast/tools/tinycbor_test/main.go
@@ -8,11 +8,75 @@ import (
 	"go.bug.st/serial"
 )
 
-type fridayMsg struct {
+type fridayPayload struct {
 	Module string `cbor:"module"`
 	Name   string `cbor:"name"`
 	Type   string `cbor:"type"`
 	Value  int    `cbor:"value"`
+}
+
+type fridayMsg struct {
+	Header  byte
+	Version byte
+	Length  uint16
+	Payload []byte
+}
+
+var msgTest = []fridayPayload{
+	// {
+	// 	Module: "gpio",
+	// 	Name:   "led",
+	// 	Type:   "cmd",
+	// 	Value:  1,
+	// },
+	// {
+	// 	Module: "gpio",
+	// 	Name:   "led",
+	// 	Type:   "cmd",
+	// 	Value:  0,
+	// },
+	// {
+	// 	Module: "sensor",
+	// 	Name:   "dps310",
+	// 	Type:   "init",
+	// 	Value:  1,
+	// },
+	// {
+	// 	Module: "sensor",
+	// 	Name:   "dps310",
+	// 	Type:   "coeff",
+	// 	Value:  1,
+	// },
+	// {
+	// 	Module: "sensor",
+	// 	Name:   "dps310",
+	// 	Type:   "meas",
+	// 	Value:  1,
+	// },
+	// {
+	// 	Module: "sensor",
+	// 	Name:   "sht4x",
+	// 	Type:   "meas",
+	// 	Value:  1,
+	// },
+	{
+		Module: "sensor",
+		Name:   "bme680",
+		Type:   "init",
+		Value:  1,
+	},
+	{
+		Module: "sensor",
+		Name:   "bme680",
+		Type:   "calib",
+		Value:  1,
+	},
+	{
+		Module: "sensor",
+		Name:   "bme680",
+		Type:   "meas",
+		Value:  1,
+	},
 }
 
 func main() {
@@ -29,42 +93,65 @@ func main() {
 	}
 	port.SetReadTimeout(time.Second * 10)
 
-	msg := fridayMsg{
-		Module: "gpio",
-		Name:   "led",
-		Type:   "cmd",
-		Value:  1,
+	for i, msg := range msgTest {
+		time.Sleep(time.Second * 5)
+
+		cborData, err := cbor.Marshal(msg)
+		if err != nil {
+			panic(err)
+		}
+
+		length := len(cborData)
+		l1 := (length >> 8) & 0xFF
+		l2 := length & 0xFF
+		fridayMsg := []byte{0x46, 0x01, byte(l1), byte(l2)}
+		fridayMsg = append(fridayMsg, cborData...)
+		// Print the message in hexadecimal format
+		for _, b := range fridayMsg {
+			fmt.Printf("%02X ", b)
+		}
+		fmt.Println()
+		n, err := port.Write(fridayMsg)
+		fmt.Println("Sent bytes:", n)
+		if err != nil {
+			panic(err)
+		}
+
+		if i == 0 {
+			continue
+		}
+
+		buf := make([]byte, 512)
+		n, err = port.Read(buf)
+		fmt.Println("Received bytes:", n)
+		if err != nil {
+			panic(err)
+		}
+
+		// Parse Friday Message
+		buf = buf[:n-1]
+		if buf[0] != 0x46 {
+			fmt.Println("Invalid header")
+		}
+
+		if buf[1] != 0x01 {
+			fmt.Println("Invalid version")
+		}
+
+		length = int(buf[2])<<8 | int(buf[3])
+		if length != n-4 {
+			fmt.Println("Invalid length")
+		}
+
+		payload := buf[4 : 4+length]
+		fmt.Printf("Payload: %X\n", payload)
+
+		var msi map[string]interface{}
+		err = cbor.Unmarshal(payload, &msi)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(msi)
 	}
 
-	cborData, err := cbor.Marshal(msg)
-	if err != nil {
-		panic(err)
-	}
-
-	length := len(cborData)
-	fmt.Printf("CBOR data length: %d bytes\n", length)
-	l1 := (length >> 8) & 0xFF
-	l2 := length & 0xFF
-	fmt.Printf("Length bytes: %02X %02X\n", l1, l2)
-	fridayMsg := []byte{0x46, 0x01, byte(l1), byte(l2)}
-	fridayMsg = append(fridayMsg, cborData...)
-
-	_, err = port.Write(fridayMsg)
-	if err != nil {
-		panic(err)
-	}
-
-	buf := make([]byte, 512)
-	n, err := port.Read(buf)
-	if err != nil {
-		panic(err)
-	}
-	buf = buf[:n-1]
-
-	var msi map[string]interface{}
-	err = cbor.Unmarshal(buf, &msi)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(msi)
 }
